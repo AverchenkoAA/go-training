95d1d00fcd0aa7354492163189433b6c6cc9fd70
Andrei Averchanka <Andrei_Averchanka@epam.com>
2020-01-08 18:03:38 +0300

    Just for fun

--- 5th_task/main.go
+++ 5th_task/main.go
@@ -4,10 +4,11 @@ import (
 	"5th_task/match"
 )
 
+//Start main function
 func main() {
 	pl1 := match.Player{Name: "Pl1", Skill: 98}
 	pl2 := match.Player{Name: "Pl2", Skill: 98}
 
-	m:=match.Match{Player1: pl1, Player2: pl2}
+	m := match.Match{Player1: pl1, Player2: pl2}
 	m.PlayMatch()
 }
